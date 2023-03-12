b6282cde20e06d222a68b01930eb8cf7819f3116
Sahil Rajput <rajputsahil.2204@gmail.com>
2023-03-12 16:04:19 +0530

    Logo and Nav Changes - Postcss and autoprefixed added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { styles } from "../style"
+import { navLinks } from "../constants/constants"
+import { logo, menu, close } from "../assets"
 
 const Navbar = () => {
+
+  const [active, setActive] = React.useState('');
+
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link to="/" className='flex items-center gap-2' onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+        }}>
+          <img src={logo} alt="logo" className='w-2 h-2 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>
+            Sahil <span className='sm:block hidden'>Computer Engineer</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
   )
 }
 

4af71c7da3a0a9cb5b9424e225efb540dadf5a31
Sahil Rajput <rajputsahil.2204@gmail.com>
2023-03-16 22:59:01 +0530

    Three JS added / Desktop Mesh Rendered

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from "../Loader"
 
 const Computers = () => {
+
+  const coumputer = useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="green" />
+      <pointLight intensity={1} color="white" />
+      <spotLight position={[-20, 10, 5]} color="aqua" angle={0.75} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
+      <primitive object={coumputer.scene} scale={0.7} position={[0, -3.5, -1.5]} rotation={[-0.01, -0.2, -0.1]} />
+    </mesh>
   )
 }
 
-export default Computers
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
+      <React.Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+        <Computers />
+      </React.Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+}
+
+export default ComputersCanvas

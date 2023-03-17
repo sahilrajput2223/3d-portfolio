67f7841253a56776b4815b17d75dde937a23220d
Sahil Rajput <rajputsahil.2204@gmail.com>
2023-03-17 20:49:39 +0530

    Actual Work History Added and Dummy Content Removed

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import xangars from "./company/xangars.png";
+import otu from "./company/otu.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +50,8 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  xangars,
+  otu,
   carrent,
   jobit,
   tripguide,
